e0cc5fa9b2bd194fbdadd3226905651690541c44
argv1 <christian.meyer@hofa.biz>
2021-01-14 01:42:12 +0100

    -

--- main.py
+++ main.py
@@ -1,27 +1,91 @@
 #!/usr/bin/env python3
 '''
-    goodXtausch helps to search for books on tauschticket.de using the selected shelves on goodreads.com and returns them as html page.
-    A valid goodreads.com API is required, this can be requested for free at https://www.goodreads.com/api
+    goodXtausch helps to search for books, games and other items on tauschticket.de based on a goodreads shelf, 
+    amazon or steam wishlist and returns the search result for each item as a table in a html page.
+    
 '''
 import argparse
-from bs4 import BeautifulSoup
+from   bs4 import BeautifulSoup
 import pandas as pd
 from   pathlib import Path
 import random
 import re
 import requests
 import time
+import sys
 
-# api keys
-goodreads_api_key = "YOUR_API_KEY"
-
-# time delay between requests 
+# Global settings
+base_path = Path(__file__).parent.absolute()
+user_agent_f = base_path / 'random_user_agent.csv'  
+result_f = base_path / 'output.html'   
 time_delay = 1
+
+def get_random_user_agent():
+    '''
+        grab a random user agent, based on https://github.com/argv1/random_user_agent
+    '''   
+    with open(user_agent_f, "r") as f:
+        lines = f.read().splitlines()
+        lines.pop(0)
+    return({'User-Agent': random.choice(lines).rsplit(',', 3)[0]})
+
+def get_items(wishlist):
+    '''
+        grab items from the choosen public accessable amazon wishlist
+    '''
+    output, headers = [], get_random_user_agent()
+
+    # fetch the url
+    r = requests.get(wishlist, headers=headers)
+    soup = BeautifulSoup(r.content, "lxml")
+
+    # Focus in the right div
+    soup = soup.find(id="content-right")
+
+    # get title from a class
+    links = soup.find_all('a', {'class': 'a-link-normal'})
+    for link in links:
+        if link.get('title') != None:
+            output.append(link.get('title'))
+
+    # remove duplicates
+    output =list(dict.fromkeys(output))
+    return output
+
+def get_games(steam_id):
+    '''
+        gets games from the choosen player ids steam wishlist
+    '''
+    import json
+
+    output, headers, n = [], get_random_user_agent(), 0
+    steam_url = f'https://store.steampowered.com/wishlist/profiles/{steam_id}/wishlistdata/?p='
+
+    # fetch the url
+    r = requests.get(steam_url, headers=headers)
+    soup = BeautifulSoup(r.text, features="lxml")
+
+    # Loop is required for wishlists with more than 100 items
+    while True:
+        data = requests.get(steam_url + str(n)).json()
+
+        # if new page contains additional items, add rge name to the list
+        if len(data) > 0:
+            output.extend(value['name'] for value in data.values())
+            n+=1
+        else:
+            break
+    return output
 
 def get_books(goodreads_shelf, goodreads_user_id):
     '''
         scrap all books from selected shelf and afterwards store the title and author for each item
     '''
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys 
+
+    browser = base_path / 'chromedriver.exe'  
     # fetch books from goodreads
     url = "https://www.goodreads.com/review/list" 
     params = {
@@ -29,21 +93,12 @@ def get_books(goodreads_shelf, goodreads_user_id):
         "id" : goodreads_user_id,
         "shelf" : goodreads_shelf,
         "per_page" : 200,
-        "key" : goodreads_api_key,
+        "key" : "depricated_api_key", #ändern
         "page" : 1
     }
-    # some user agent headers
-    user_agent_list = [
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-    ]
-    
-    #Select random header
-    headers = {'User-Agent': random.choice(user_agent_list)}
 
-    books = []
+    # some user agent headers
+    books, headers = [], get_random_user_agent()
 
     while True:
         resp = requests.get(url, params)
@@ -74,25 +129,34 @@ def get_books(goodreads_shelf, goodreads_user_id):
         output.append(book_data)
     return output
 
-def search_tausch(row):
+def search_tausch(row, setting):
     '''
         search for book(s) at tauschticket.de
     '''
     # special characters to remove from search at tauschticket.de
-    ignore_characters = ['`', '^', '[', ']', '\\', '<', '–', '™']]
+    ignore_characters = ['`', '^', '[', ']', '\\', '<', '–', '™']  #, '®', '-'
 
     s = pd.Series(dtype="object")
     
     # get and parse search results
     url = "https://www.tauschticket.de/suche/"
-    search_string = "{} {}".format(row["title"], row["author"])
+    if(setting == "b"):
+        search_string = f'{row["title"]} {row["author"]}'
+    else:
+        search_string = f'{row["title"]}'
     for char in ignore_characters:
         search_string = search_string.replace(char, "")
+    
     params = {
-        "keywords" :  search_string,
-        "kategorie" : ""
-    }
-    resp = requests.get(url, params)
+            "keywords" :  search_string,
+            "kategorie" : ""
+        }
+    if(setting == "b"):
+        params.update({"kategorie" : "buch"})
+    elif(setting == "s"):
+        params.update({"kategorie" : "pcgame"})
+  
+    resp = requests.get(url, params=params)
     assert resp.status_code == 200, "Exception accessing tauschticket.de"
     soup = BeautifulSoup(resp.text, "lxml")
     
@@ -102,12 +166,13 @@ def search_tausch(row):
     # regular expression is used here to do spacing issues
     if re.search("Keine\s+?Angebote\s+?in", headline_text):
         s["results"] = 0
-    elif re.search("Angebote\s+?mit", headline_text):
-        pattern = "^(\d+)\s+?Angebote\s+?mit.*?\s+?gefunden"
+    elif re.search("Angebote.*?mit", headline_text):
+        pattern = "^(\d+)\s+?Angebote.*?mit.*?\s+?gefunden"
         result = re.search(pattern, headline_text)
-        if result: s["results"] = result.groups()[0]
+        if result: 
+            s["results"] = result.groups()[0]
     else:
-        raise Exception("Error parsing headline of tauschticket, perhaps the site has been updated. ")
+        raise Exception("Error parsing headline of tauschticket, perhaps the site has been updated.")
     
     # save the url
     s["url"] = resp.url
@@ -119,28 +184,39 @@ def search_tausch(row):
 def main():
     # Initiate the parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--shelf', help='Enter enter name of the shelf', type=str, required=True) 
-    parser.add_argument('-u', '--user_id', help='Enter enter your goodreads user id', type=str, required=True) # user id needs to correspondent to goodreads api key owner
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-a', '--amazon', help='Enter (public accessable) amazon wishlist url', type=str)
+    group.add_argument('-s', '--shelf', help='Enter enter name of the shelf', type=str) 
+    group.add_argument('-p', '--user_id', help='Enter enter your goodreads user id', type=str) # user id needs to correspondent to provided shelf
+    group.add_argument('-u', '--steam_id', help='Enter steam id', type=str)
     args = parser.parse_args()
-    goodreads_shelf   = args.shelf
-    goodreads_user_id = args.user_id
-   
-    # create a pandas series 
-    df = pd.DataFrame(data=get_books(goodreads_shelf, goodreads_user_id))
-    
+
+    if (args.amazon):
+        ama_wlist   = args.amazon
+        df = pd.DataFrame(data=get_items(ama_wlist), columns=["title"])
+        setting = "a"
+    elif(args.shelf and args.user_id):
+        goodreads_shelf   = args.shelf
+        goodreads_user_id = args.user_id
+        df = pd.DataFrame(data=get_books(goodreads_shelf, goodreads_user_id))
+        setting = "b"
+    elif(args.steam_id):
+        steam_id = args.steam_id
+        df = pd.DataFrame(data=get_games(steam_id), columns=["title"])
+        setting = "s"
+    else:
+        print("Missing arguments, please run main.py -h for help")
+        sys.exit(1)
+
     # pandas operation to apply 
-    tausch_df = df.apply(search_tausch, axis=1)
+    tausch_df = df.apply(search_tausch, args=(setting), axis=1)
     combined_df = pd.concat([df, tausch_df], axis=1)
-    
-    # Define path and filename
-    base_path   = Path('H:\OneDrive\Programme\_current\goodXtausch')  #adjust
-    result_file = base_path / 'output.html'            
     
     # return an html table
     pd.set_option('display.max_colwidth', None)
     combined_df['link'] = "<a href='"+combined_df['url']+"'>"+combined_df['title'].astype(str).str[0:15]+"...</a>"
     html_table = combined_df.to_html(escape=False)
-    with open(result_file, "w", encoding="utf-8") as f:
+    with open(result_f, "w", encoding="utf-8") as f:
         for line in html_table:
             f.write(line)
             
--- readme.md
+++ readme.md
@@ -1,83 +1,73 @@
 ![logo](https://github.com/argv1/goodXtausch/blob/master/images/logo.PNG)
-
+ 
 ## goodXtausch
 ======================
-
-goodXtausch helps to search for books on tauschticket.de using the selected shelves on goodreads.com and returns the search result for each book as a table in a html page.
-
-
+ 
+goodXtausch helps to search for books, games and other items on tauschticket.de based on a goodreads shelf, amazon or steam wishlist and returns the search result for each item as a table in a html page.
+ 
+Since, as of December 8th 2020, Goodreads no longer issues new developer keys for the public developer API and plans to retire the current version of these tools (more info [here](https://help.goodreads.com/s/article/Does-Goodreads-support-the-use-of-APIs)), I desided to use selenium to solve this issue.
+ 
 ![logo](https://github.com/argv1/goodXtausch/blob/master/images/output.PNG)
-
-
+ 
+ 
 ## Table of content
-
+ 
 - [Installation](#installation)
-
-- [Setup](#setup)
-    - [API-key](#API-key)
     - [User-ID](#User-ID)
 	- [Shelf-ID](#Shelf-ID)
 - [Usage](#usage)	
 - [License](#license)
 - [Outlook](#outlook)
-
+ 
 ## Installation
-
-goodXtausch use the packaging tool Pipenv.
-If you are not already use Pipenv, please run 
+ 
+You could either use pip
 ```bash
-pip install pipenv
+pip3 install -r requirements.txt
 ```
-
-After you successful installed pipenv, please run
+ 
+or pipenv
 ```bash
 pipenv install --ignore-pipfile
 ```
 to install the required packages
-
-## setup
-
-### API-key
-
-1. Register for a free [Goodreads API key](https://www.goodreads.com/api/keys) in order to access your list of books.
-2. Ensure your [Goodreads account privacy settings](https://www.goodreads.com/user/edit?tab=settings) allow for access to your shelves via the API.
-    - Set `Who can view my profile:` to `anyone (including search engines)`
-    - Check the box `Allow partners of Goodreads to display my reviews`
-
-
-Enter this api key here:
-```python
-goodreads_api_key = YOUR-API-KEY
-```
-
+ 
+ 
 ### User-ID
-
-Browser to your goodreads.com profile to find your user_id
+ 
+Go to the relevant goodreads.com profile to find the corresponding user_id
 ![User ID](https://github.com/argv1/goodXtausch/blob/master/images/goodreads_user_id.PNG)
-
-
+ 
+ 
 ### Shelf-ID
-
+ 
 ![Shelf](https://github.com/argv1/goodXtausch/blob/master/images/goodreads_shelf.PNG)
-
-You can find your shelfs on the left side of your profile page
-
-
+ 
+Select one of the available shelves on the left side of the profile page.
+  
 ## usage
-After you API key, run main.py with the two required arguments -s followed by your shelf name and -u followed by your username (corresponding to the goodreads api key owner)
+Run the main.py with the required argument(s)
+(Use main.py -h for a list of all options)
+ 
+-a followed by your amazon wishlist url
+or
+-s followed by the goodreads.com shelf and
+-p followed by the corresponding goodreads.com profile
+or
+-u followed by your steam UserId
 
-Usage: main.py -s cooking -u 13370123<p>
-Use main.py -h for a list of all options
-
+Usage: main.py -u STEAM_ID<p>
+ 
 ## License
-
+ 
 This code is licensed under the [GNU General Public License v3.0](https://choosealicense.com/licenses/gpl-3.0/). 
 For more details, please take a look at the [LICENSE file](https://github.com/argv1/goodXtausch/blob/master/LICENSE).
-
-
+ 
+ 
 ## outlook
-
-- [x] Possibility to enter user_id and shelf on the command line
-- [ ] Possibility to scrap any shelf (here by no further use of the goodreads.com API.)
+ 
+- [x] Possibility to use command line
+- [x] Possibility to scrap any shelf (here by no further use of the goodreads.com API.)
+- [x] Add steam and amazon wishlist
 - [ ] Add search feature for ISBN (https://www.goodreads.com/api/index#book.show_by_isbn)
 - [ ] If ISBN is given for hardcover book, search for additional ISBNs like for the paperback version
