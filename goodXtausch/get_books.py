088a6178e8e1e7e63b9ec4593d5b923879315fec
argv1 <github@hofa.biz>
2022-11-25 17:25:39 +0100

    Update get_books.py

--- goodXtausch/get_books.py
+++ goodXtausch/get_books.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def goodreads(goodreads_shelf_export_f):
     '''
-        get all books from your shelf (need to be downloaded manually upfront)
+        get all books from your shelf (due to deprecated goodreads API list need to be downloaded manually upfront)
         in the future missing ISBN should be added, also soft and hardcover version of the same book
     '''
 
